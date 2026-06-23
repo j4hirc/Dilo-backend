eea7dabf96e3ac034b11069be10b8a091bc0c262
j4hirc <112521047+j4hirc@users.noreply.github.com>
2026-06-23 17:39:35 -0500

    correccion de errores

--- src/main/java/com/example/dilo/DiloBackend/controller/AuthController.java
+++ src/main/java/com/example/dilo/DiloBackend/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.example.dilo.DiloBackend.dto.response.UsuarioResponseDTO;
 import com.example.dilo.DiloBackend.service.RegistroService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,16 +20,14 @@ public class AuthController {
     private final RegistroService registroService;
 
     @PostMapping(value = "/registro", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<?> registrarUsuario(
+    public ResponseEntity<UsuarioResponseDTO> registrarUsuario(
             @RequestPart("datos") RegisterUserDTO datosUsuario,
             @RequestPart(value = "foto", required = false) MultipartFile foto) {
 
-        try {
             UsuarioResponseDTO response = registroService.registroUsuario(datosUsuario, foto);
-            return ResponseEntity.ok(response);
+            return new ResponseEntity<>(response, HttpStatus.CREATED);
 
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body("Error en el registro: " + e.getMessage());
-        }
     }
+
+
 }
