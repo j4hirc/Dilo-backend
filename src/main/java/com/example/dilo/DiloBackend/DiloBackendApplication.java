092a270e072a64bfdf1cacebfe80419f7f92c110
j4hirc <112521047+j4hirc@users.noreply.github.com>
2026-06-23 14:54:26 -0500

    FIRST COMMIT

---
+++ src/main/java/com/example/dilo/DiloBackend/DiloBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.dilo.DiloBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiloBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiloBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dilo/DiloBackend/DiloBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dilo.DiloBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiloBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
