c231a1a3e5d097b70d8eb01d053aff4b0c00e944
j4hirc <112521047+j4hirc@users.noreply.github.com>
2026-07-14 11:23:29 -0500

    cambios miembro del negocio

--- src/main/java/com/example/dilo/DiloBackend/dto/response/MiembroNegocioResponseDTO.java
+++ src/main/java/com/example/dilo/DiloBackend/dto/response/MiembroNegocioResponseDTO.java
@@ -2,9 +2,11 @@ package com.example.dilo.DiloBackend.dto.response;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
+@Data
 public class MiembroNegocioResponseDTO {
 
     private Long nombreUsuario;
